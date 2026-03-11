999be4271631a783fe75e9d85d7b7c485527f727
Charles Eaton <32652102+CEaton13@users.noreply.github.com>
2026-03-11 16:49:44 -0500

    Initialize Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/com/app/eventnexus/EventnexusApplication.java
@@ -0,0 +1,13 @@
+package com.app.eventnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventnexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventnexusApplication.class, args);
+	}
+
+}
